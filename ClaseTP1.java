e3500ad652952198ae85fc4ee750412a2035360a
Andres Juarez <102993598+Andres3345@users.noreply.github.com>
2023-03-07 10:38:28 -0300

    Add files via upload

---
+++ ClaseTP1.java
@@ -0,0 +1,70 @@
+package paqueteTP1;
+
+public class ClaseTP1 {
+
+	public static void main(String[] args) {
+		/*Ejercicio 1
+		int x = 5, y = 14;
+		while(y >= x) {
+			if(x <= y) {
+			System.out.println(x);
+			}
+			x++;
+		}
+		
+		Ejercicio 2
+		int x = 5, y = 14;
+		while (x <= y) {
+			if(x % 2 == 0) {
+				System.out.println(x);
+			}
+			x++;
+		}*/
+		/*Ejercicio 3
+		int x = 5, y = 14;
+		boolean MostrarPares = false;
+		while(x <= y) {
+			if(MostrarPares == true) {
+				if(x % 2 == 0) {
+					System.out.println(x);
+				}
+				
+			}else {
+				if(x % 2 != 0) {
+					System.out.println(x);
+				}
+			}
+			x++;
+		}*/
+		/*int x = 14, y = 5;
+		for(int i = x; i >= y; i--) {
+			System.out.println("el valor de i es: " + i);
+		}*/
+		/*Ejercicio Final
+		float ingresoM = 500000, ingresoMax = 490000;
+		boolean MasTresVehiculos = false, MasTresInmuebles = true, BarcoAeronave = false;
+		 if(ingresoM <= ingresoMax) {
+			if(MasTresVehiculos == true ) {
+				System.out.println("La Familia es de Clase A");
+			}else {
+			if(MasTresInmuebles == true) {
+				System.out.println("La Familia es de Clase A");
+			}else {
+			if(BarcoAeronave == true) {
+				System.out.println("La Familia es de Clase A");
+			}else {							
+				   System.out.println("la familia es clase B"); 
+				 }
+			
+			
+			  }
+			 }
+			}else {
+				System.out.println("La Familia es de Clase A");
+			}*/
+
+		//TP1 Completo
+
+	}
+
+}
