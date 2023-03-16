050b2dcb021e91c079642a10c43a54502171d624
Andres Juarez <102993598+Andres3345@users.noreply.github.com>
2023-03-16 10:57:12 -0300

    Add files via upload

---
+++ metodos.java
@@ -0,0 +1,26 @@
+package numeros;
+
+public class metodos {
+	public double sumar (double n1 , double n2) {
+		double resultado = 0;
+		resultado = n1 + n2;
+		return resultado; 
+	}
+	
+	public double restar(double n1 , double n2) {
+		double resultado = 0;
+		resultado = n1 - n2;
+		return resultado;
+	}
+	public double multiplicar(double n1 , double n2) {
+		double resultado = 0; 
+		resultado = n1 * n2;
+		return resultado;
+	}
+	public double dividir (double n1 , double n2) {
+		double resultado = 0;
+		resultado = n1 / n2;
+		return resultado;
+	}
+
+}
